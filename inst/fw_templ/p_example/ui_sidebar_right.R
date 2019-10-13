9fe271e8f8fcc8f1a49c863692b65a751a00a727
ginberg <ginberg@gmail.com>
2019-10-13 20:56:12 +0200

    update example

--- inst/fw_templ/p_example/ui_sidebar_right.R
+++ inst/fw_templ/p_example/ui_sidebar_right.R
@@ -27,26 +27,16 @@ tab1 <- rightSidebarTabContent(
     id = 1,
     icon = "desktop",
     title = "Tab 1",
-    active = TRUE,
-    sliderInput(
-        "obs", 
-        "Number of observations:",
-        min = 0, max = 1000, value = 500
-    )
-)
+    active = TRUE)
 
 tab2 <- rightSidebarTabContent(
     id = 2,
-    title = "Tab 2",
-    textInput("caption", "Caption", "Data Summary")
-)
+    title = "Tab 2")
 
 tab3 <- rightSidebarTabContent(
     id = 3,
     title = "Tab 3",
-    icon = "paint-brush",
-    numericInput("obs", "Observations:", 10, min = 1, max = 100)
-)
+    icon = "paint-brush")
 
 # -- Register Basic Elements in the ORDER SHOWN in the UI
 add_ui_sidebar_right(list(tab1, tab2, tab3))
