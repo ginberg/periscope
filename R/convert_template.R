caf148ad0b5c618b39b5128b13dd074b0d66cebc
Ger <ger@aggregate-genius.com>
2020-01-13 15:19:17 +0000

    update conversion

--- R/convert_template.R
+++ R/convert_template.R
@@ -29,17 +29,25 @@ remove_reset_button <- function(location) {
         else {
             usersep <- .Platform$file.sep
             
-            # overwrite ui.R
-            writeLines(gsub("fw_create_sidebar\\(", "fw_create_sidebar\\(resetbutton = FALSE", 
-                            readLines(con = paste(location, "ui.R", sep = usersep))), 
-                       con = paste(location, "ui.R", sep = usersep))
+            files_updated <- c()
+            # update ui if needed
+            ui_content <- readLines(con = paste(location, "ui.R", sep = usersep))
+            if (!any(grepl("fw_create_sidebar\\(resetbutton", ui_content))) {
+                writeLines(gsub("fw_create_sidebar\\(", "fw_create_sidebar\\(resetbutton = FALSE", ui_content), 
+                           con = paste(location, "ui.R", sep = usersep))
+                files_updated <- c(files_updated, "ui.R")
+            }
             
+            # should we remove the (possible) reset button message as well?
             # if (sampleapp && !resetbutton) {
             #     file.rename(paste(targetdir, "ui_sidebar_no_reset.R", sep = usersep), paste(targetdir, "ui_sidebar.R", sep = usersep))
             # }
             
-            message("Remove reset button conversion was successful.")
-            message("Files updated: ui.R")
+            if (length(files_updated) > 0) {
+                message(paste("Remove reset button conversion was successful. File(s) updated:",  paste(files_updated, collapse = ",")))
+            } else {
+                message("Reset button already removed, no conversion needed")  
+            }
         }
     },
     warning = function(w) {
